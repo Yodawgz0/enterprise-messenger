0ec1e193702067404a51b87d9eccf9f07dc5ca2e
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-29 20:12:39 -0400

    adding overlay of login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,6 +3,20 @@
 
 <template>
   <main>
-    Hello
+    <div class=" bg-black flex flex-col h-screen ">
+      <h1 class="text-white">Login</h1>
+      <div class="p-8 mx-96 flex flex-col justify-evenly mt-48">
+        <div class="flex flex-row font-bold justify-evenly mb-5 ">
+          <p class="text-yellow-400">Username :</p>
+          <input class="border-solid border-2 rounded-lg text-center" />
+        </div>
+        <div class="flex flex-row font-bold justify-evenly ">
+          <p class="text-yellow-400">Password :</p>
+          <input type="password" class=" border-solid border-2 rounded-lg" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped></style>
