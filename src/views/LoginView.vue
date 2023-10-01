6ff7640d221ceabe79f179a99cb32227815f55f4
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-01 15:25:09 -0400

    usign v-model

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,11 @@
+
 <script setup lang="ts">
+import { ref, type Ref } from 'vue';
+const userName: Ref<string> = ref("")
+
+const loginHandler = () => {
+  console.log(userName.value)
+}
 </script>
 
 <template>
@@ -8,13 +15,13 @@
       <div class="p-8 mx-96 flex flex-col justify-evenly mt-20">
         <div class="flex flex-row font-bold justify-evenly mb-5 ">
           <p class="text-yellow-400">Username :</p>
-          <input class="border-solid border-2 rounded-lg text-center" />
+          <input v-model="userName" class="border-solid border-2 rounded-lg text-center" />
         </div>
         <div class="flex flex-row font-bold justify-evenly ">
-          <p class="text-yellow-400">Password :</p>
+          <p class="text-yellow-400" ref="passwordInput">Password :</p>
           <input type="password" class=" border-solid border-2 rounded-lg text-center" />
         </div>
-        <button class="mt-11 bg-yellow-600 rounded-md py-1 w-64 font-bold ms-56">Login</button>
+        <button @click='loginHandler' class="mt-11 bg-yellow-600 rounded-md py-1 w-64 font-bold ms-56">Login</button>
       </div>
     </div>
   </main>
