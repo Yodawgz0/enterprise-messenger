0611b7f76f9ded0d087b8d19b1cbce5e2c7d15e6
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-22 21:16:17 -0400

    adding dashboard

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class=" bg-black h-[95.5vh]">
+        <p class=" text-white text-center font-bold">EnChat</p>
+        <hr class=" mt-4" />
+    </main>
+</template>
+
+<style scoped></style>
