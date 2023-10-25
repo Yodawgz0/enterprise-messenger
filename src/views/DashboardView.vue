5af0861eeb744d1754cf73fc195e1ab1dc3954e4
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-25 10:14:39 -0400

    basic sturcture of dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,9 +4,27 @@
 </script>
 
 <template>
-    <main class=" bg-black h-[95.5vh]">
-        <p class=" text-white text-center font-bold">EnChat</p>
-        <hr class=" mt-4" />
+    <main class=" bg-black h-[100vh]">
+        <p class=" text-white text-center font-bold pt-4 text-2xl">EnChat</p>
+        <hr class=" border-yellow-600 mt-4" />
+        <div class=" flex flex-row">
+            <div class=" w-[20vw] border-2 border-black border-r-yellow-600 h-[90vh]">
+                <div>
+                    <p class="text-white text-center p-3 ">
+                        Online Users</p>
+                </div>
+            </div>
+            <div>
+                <div class="text-white h-[75vh]">
+                    hello
+                </div>
+                <hr class="border-2 w-[80vw] border-white" />
+                <input class="mx-3 my-4 w-[70vw] rounded outline-none p-2" />
+            </div>
+
+        </div>
+
+
     </main>
 </template>
 
