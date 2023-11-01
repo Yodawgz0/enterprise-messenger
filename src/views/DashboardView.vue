64fc6f2f938bab2873ca612b8cd1d30980e2e626
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-10-31 22:58:48 -0400

    importing icon

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,10 @@
 
 <script setup lang="ts">
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+library.add(faPaperPlane)
+
 
 </script>
 
