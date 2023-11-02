1fa3a2f7d120da51afaeac8d466ed38e2e6646b2
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-11-01 21:56:04 -0400

    adding render if that data

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -9,28 +9,33 @@ const dataChat: { [key: string]: string }[] = [{ p1: "Asdhley" }, { p1: "Asdhley
 </script>
 
 <template>
-    <main class=" bg-black h-[100vh]">
-        <p class=" text-white text-center font-bold pt-4 text-2xl">EnChat</p>
-        <hr class=" border-yellow-600 mt-4" />
-        <div class=" flex flex-row">
-            <div class=" w-[20vw] border-2 border-black border-r-yellow-600 h-[90vh]">
-                <div>
-                    <p class="text-white text-center p-3 ">
-                        Online Users</p>
-                </div>
-            </div>
-            <div>
-                <div class="text-white h-[75vh]">
-                    hello
-                </div>
-                <hr class="border-2 w-[80vw] border-white" />
-                <input class="mx-3 my-4 w-[70vw] rounded outline-none p-2" />
-            </div>
-
+  <main class=" bg-black h-[100vh]">
+    <p class=" text-white text-center font-bold pt-4 text-2xl">EnChat</p>
+    <hr class=" border-yellow-600 mt-4" />
+    <div class=" flex flex-row">
+      <div class=" w-[20vw] border-2 border-black border-r-yellow-600 h-[90vh]">
+        <div>
+          <p class="text-white text-center p-3 ">
+            Online Users</p>
         </div>
+      </div>
+      <div>
+        <div class="h-[75vh]">
+          <div class="h-[75vh]">
+            <div v-for="(item, index) in dataChat" :key="index">
+              <p v-if="item.p1" class="text-white bg-red-950 rounded w-[30vw] m-3 p-3">{{ item.p1 }}</p>
+              <p v-else-if="item.p2" class="text-white bg-amber-400 rounded w-[30vw] m-3 p-3 float-right">{{ item.p2 }}
+              </p>
+            </div>
 
-
-    </main>
+          </div>
+        </div>
+        <hr class="border-2 w-[80vw] border-white" />
+        <input class="mx-3 my-4 w-[70vw] rounded outline-none p-2" />
+        <font-awesome-icon class="text-white w-6 h-6 pt-7 ms-5 cursor-pointer" icon="paper-plane" />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
