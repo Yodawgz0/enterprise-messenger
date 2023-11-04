28679d1279cbe95c1879bdddd49702af1fff0456
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-11-03 20:56:09 -0400

    latest message scroll

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,17 +3,21 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 library.add(faPaperPlane)
 const chatInput = ref('');
 const dataChat = ref([{ p1: 'Asdhley' }, { p1: 'Asdhley' }, { p2: 'Asdhley' }]);
-
+const chatContainer = ref();
 function addChatMessage() {
   if (chatInput.value) {
     dataChat.value.push({ p2: chatInput.value });
     chatInput.value = '';
   }
+  nextTick(() => {
+    chatContainer.value.scrollTop = chatContainer.value.scrollHeight;
+  });
 }
+
 </script>
 
 <template>
@@ -28,17 +32,16 @@ function addChatMessage() {
         <p class="text-white text-center p-3 pl-8 relative">
           Online Users
         </p>
-        </div>
       </div>
       <div>
-        <div class="h-[75vh]">
-          <div class="h-[75vh]">
-            <div v-for="(item, index) in dataChat" :key="index">
-              <div class="flex flex-col">
-                <p v-if="item.p1" class="text-white bg-red-950 rounded w-[30vw] m-3 p-3">{{ item.p1 }}</p>
-                <div v-else-if="item.p2" class="flex justify-end">
-                  <p class="text-white bg-amber-400 rounded w-[30vw] m-3 p-3 text-right">{{ item.p2 }}</p>
-                </div>
+        <div
+          class="h-[75vh] overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
+          ref="chatContainer">
+          <div v-for="(item, index) in dataChat" :key="index">
+            <div class="flex flex-col">
+              <p v-if="item.p1" class="text-white bg-red-950 rounded w-[30vw] m-3 p-3">{{ item.p1 }}</p>
+              <div v-else-if="item.p2" class="flex justify-end">
+                <p class="text-white bg-amber-400 rounded w-[30vw] m-3 p-3 text-right">{{ item.p2 }}</p>
               </div>
             </div>
           </div>
